db049d38197af992d253048e7f67774200e138f6
txzh007 <txzh007@gmail.com>
2019-05-14 17:55:18 +0800

    add new solution

--- main.c
+++ main.c
@@ -3,33 +3,50 @@
 #include <string.h>
 
 
-char *longestCommonPrefix(char strs[3][6], int strsSize) {
-    int maxLen = strlen(strs[0]);
-    char *result = NULL;
-    for (int i = 0; i < maxLen; i++) {
-
-        for (int j = 1; j < strsSize; j++) {
-            if (strs[j][i] != strs[0][i]) {
-                printf("i=%d\n", i);
-                result = (char *) malloc(sizeof(char) * i);
-                printf("res length is %d\n", strlen(result));
-                for (int k = 0; k < i; k++) {
-                    result[k] = strs[0][k];
-
-                }
-                result[i]='\0';
-                return result;
-            }
+int max(int a, int b) { return a > b ? a : b; }
+char *addBinary(char *a, char *b) {
+    int aLength = strlen(a);
+    int bLength = strlen(b);
+    int length = max(aLength, bLength);
+    char *arr = (char *) malloc(sizeof(char) * (length + 2));
+    int tmp = 0;
+    for (int i = 0; i <= length; i++) {
+        arr[i] = '0';
+        if (aLength - i - 1 >= 0) {
+            arr[i] += (a[aLength - i - 1] - '0');
+        }
+        if (bLength - i - 1 >= 0) {
+            arr[i] += (b[bLength - i - 1] - '0');
+        }
+        arr[i] += tmp;
+        if (arr[i] >= '2') {
+            arr[i] = '0' + ((arr[i] - '0') % 2);
+            tmp = 1;
+        } else {
+            tmp = 0;
         }
     }
-    return result;
-
+    if (arr[length] == '1')
+        arr[length + 1] = '\0';
+    else
+        arr[length] = '\0';
+    length = strlen(arr);
+    for (int i = 0; i < length / 2; i++) {
+        char tmp = arr[i];
+        arr[i] = arr[length - i - 1];
+        arr[length - i - 1] = tmp;
+    }
+    if (arr[0] == '0')
+        return "0";
+    return arr;
 }
 
 int main() {
-    char str[3][6] = {"flower", "flow", "flight"};
+    char *a = (char *) malloc(sizeof(char) * 4);
+    char *b = (char *) malloc(sizeof(char) * 4);
+    strcpy(a, "1111");
+    strcpy(b, "1111");
+    printf("%s\n", addBinary(a, b));
 
-    printf("%s\n", longestCommonPrefix(str, 3));
-    printf("%d", strlen(longestCommonPrefix(str, 3)));
     return 0;
 }
