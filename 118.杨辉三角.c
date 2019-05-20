d342184c69bd2a1dfcea3ae04cc0cc854896559d
txzh007 <txzh007@gmail.com>
2019-05-20 18:07:12 +0800

    something

--- 118.杨辉三角.c
+++ 118.杨辉三角.c
@@ -4,15 +4,28 @@
  * [118] 杨辉三角
  */
 
-
 /**
  * Return an array of arrays of size *returnSize.
  * The sizes of the arrays are returned as *returnColumnSizes array.
- * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ * Note: Both returned array and *columnSizes array must be malloced, assume
+ * caller calls free().
  */
-int** generate(int numRows, int* returnSize, int** returnColumnSizes){
+int **generate(int numRows, int *returnSize, int **returnColumnSizes) {
+    *returnSize = numRows;
+    int **result = (int **)malloc(sizeof(int) * numRows);
+    for (int i = 1; i <= numRows; i++) {
+        *result[i - 1] = (int *)malloc(sizeof(int) * i);
+        *returnColumnSizes = i;
+    }
+    for (int i = 0; i <= numRows; i++) {
+        for (int j = 0; j < i; j++) {
+            if (j == i || j == 0) {
+                result[i][j] = 1;
+            } else {
+                result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
+            }
+        }
+    }
 
+    return result;
 }
-
-
-
