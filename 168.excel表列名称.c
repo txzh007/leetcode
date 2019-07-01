021da363c9e46c5f2036282d0d99194491f2f80a
txzh007 <txzh007@gmail.com>
2019-07-01 17:57:01 +0800

    add problem No.168

--- 168.excel表列名称.c
+++ 168.excel表列名称.c
@@ -4,4 +4,21 @@
  * [168] Excel表列名称
  */
 
-char *convertToTitle(int n) { char *res; }
+char *convertToTitle(int n) {
+    if (n < 0) {
+        return "";
+    }
+    char *res = malloc(10);
+    int len = 0;
+    while (n > 0) {
+        res[len++] = 'A' + ((n - 1) % 26);
+        n = (n - 1) / 26;
+    }
+    res[len] = '\0';
+    for (int i = 0; i < len / 2; i++) {
+        char tmp = res[i];
+        res[i] = res[len - 1 - i];
+        res[len - 1 - i] = tmp;
+    }
+    return res;
+}
